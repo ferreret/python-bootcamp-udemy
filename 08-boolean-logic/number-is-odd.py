e5c741a9cce41b0e6e7094714882fc6afbcbb7e0
nicolas <ferreret@gmail.com>
2020-05-04 14:11:47 +0200

    Conditionals

---
+++ 08-boolean-logic/number-is-odd.py
@@ -0,0 +1,13 @@
+# NO TOUCHING ======================================
+from random import randint
+num = randint(1, 1000)  # picks random number from 1-1000
+# NO TOUCHING ======================================
+
+
+# YOUR CODE GOES HERE vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+# print(num)
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
+# YOUR CODE GOES HERE ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
