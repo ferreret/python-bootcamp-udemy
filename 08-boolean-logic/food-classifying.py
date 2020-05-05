586fcd1d65e972c3a4baae25467b3b7d258e6665
nicolas.barcelo.tecnomedia@hotmail.com <nicolas.barcelo.tecnomedia@hotmail.com>
2020-05-05 15:54:46 +0200

    boolean 2

---
+++ 08-boolean-logic/food-classifying.py
@@ -0,0 +1,16 @@
+# NO TOUCHING ============================================
+from random import choice
+food = choice(['apple','grape', 'bacon', 'steak', 'worm', 'dirt'])
+# NO TOUCHING =============================================
+
+
+# YOUR CODE GOES HERE vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+if food == "apple" or food == "grape":
+    print("fruit")
+elif food == "bacon" or food == "meat":
+    print("meat")
+else:
+    print("yuck")
+
+
+# YOUR CODE GOES HERE ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
---
+++ 08-boolean-logic/truthiness.py
@@ -0,0 +1,8 @@
+print("Enter your favorite animal")
+
+animal = input()
+
+if animal:
+    print(animal + " is my favorite animal too!")
+else:
+    print("You didn't enter any animal")
