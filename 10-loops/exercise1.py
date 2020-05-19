dcb903e54d7dbf33296ea05653c6ed42bcc3157e
nicolas <ferreret@gmail.com>
2020-05-19 22:58:43 +0200

    repeater

---
+++ 10-loops/exercise1.py
@@ -0,0 +1,10 @@
+# Add up all odd numbers between 10 and 20
+# Store the result in x:
+x = 0
+
+# YOUR CODE GOES HERE:
+for number in range(11, 20, 2):
+    x += number
+
+print(x)
+
---
+++ 10-loops/repeater.py
@@ -0,0 +1,5 @@
+print("How many times do I have to tell you?")
+times = input()
+times = int(times)
+for i in range(times):
+    print(f" Time {i+1}: CLEAN UP YOUR ROOM!")
